4ce3153a76ab274232bcc7b617dbfc54ced22d41
Alan Grunberg <alandgh@gmail.com>
2022-06-28 19:21:00 +0800

    adding sarcasticize function to lib.py with test

--- mytoolbox/lib.py
+++ mytoolbox/lib.py
@@ -1,2 +1,15 @@
 def try_me():
     return "testing testing testing 123 123 123"
+
+def sarcasticize(text):
+    char_list = []
+    count = 0
+    for char in text:
+        if count % 2 == 0:
+            char_list.append(char.lower())
+        else:
+            char_list.append(char.upper())
+        count += 1
+    sarcasticized = ''.join(char_list)
+    print(sarcasticized)
+    return sarcasticized
--- tests/test_lib.py
+++ tests/test_lib.py
@@ -1,4 +1,7 @@
-from mytoolbox.lib import try_me
+from mytoolbox.lib import sarcasticize, try_me
 
 def test_try_me():
     assert type(try_me()) == str
+
+def test_sarcasticize():
+    assert len(sarcasticize('hello')) == len('hello')
